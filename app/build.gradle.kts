fdac0bc11e035ae7ae5294116c3165c5ee0d9c67
Tony0619-29 <antoniomed@comunidad.unam.mx>
2025-04-29 07:42:33 -0600

    Se Agrego a nivel de configueracion firabase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,10 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     //Implementacion de Lottie
     implementation(libs.lottie)
+    //Implementacion de Firebase
+    implementation(platform(libs.firebase))
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.auth.ktx)
+
 }
