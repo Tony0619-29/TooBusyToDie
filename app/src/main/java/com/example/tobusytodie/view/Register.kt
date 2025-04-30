4c02759952d261a5108b0eea6cecb63c6c19caca
Tony0619-29 <antoniomed@comunidad.unam.mx>
2025-04-29 22:49:47 -0600

    Se agrego el viewModel y view del register

--- app/src/main/java/com/example/tobusytodie/view/Register.kt
+++ app/src/main/java/com/example/tobusytodie/view/Register.kt
@@ -1,42 +1,94 @@
 package com.example.tobusytodie.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.navigation.fragment.findNavController
 import com.example.tobusytodie.R
+import com.example.tobusytodie.utils.FragmentCommunicator
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.tobusytodie.databinding.FragmentRegisterBinding
+import com.example.tobusytodie.viewModel.RegisterViewModel
 
 class Register : Fragment() {
     private var _binding: FragmentRegisterBinding? = null
+    private val viewModel by viewModels<RegisterViewModel>()
+    var isValid: Boolean = false
     private val binding get() = _binding!!
+    private lateinit var communicator: FragmentCommunicator
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        communicator = requireActivity() as MainActivity
+        setupView()
         return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.imageButton.setOnClickListener {
+        binding.flechaLogin.setOnClickListener {
             findNavController().navigate(R.id.action_register2_to_login2)
         }
 
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+
+
+
+    private fun setupView() {
+        binding.flechaLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+
+        binding.buttonRegistrar.setOnClickListener {
+            viewModel.requestSignUp(
+                binding.tietEmail.text.toString(),
+                binding.tietPassword.text.toString()
+            )
+        }
+
+
+        //valida que se ingresen los datos
+        binding.tietEmail.addTextChangedListener {
+            if (binding.tietEmail.text.toString().isEmpty()) {
+                binding.textInputLayout4.error = "Por favor introduce un correo"
+                isValid = false
+            } else {
+                isValid = true
+            }
+        }
+
+        binding.tietPassword.addTextChangedListener {
+            if (binding.tietPassword.text.toString().isEmpty()) {
+                binding.textInputLayout5.error = "Por favor introduce un correo"
+                isValid = false
+            } else {
+                isValid = true
+            }
+        }
+
+    }
+    private fun setupObservers() {
+        viewModel.loaderState.observe(viewLifecycleOwner) { loaderState ->
+            communicator.showLoader(loaderState)
+        }
+
+        viewModel.validRegister.observe(viewLifecycleOwner) { validRegister ->
+            if (validRegister) {
+                findNavController().navigate(R.id.action_register2_to_login2)
+            }
+        }
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance() = Register()
-    }
 }
--- app/src/main/java/com/example/tobusytodie/viewModel/RegisterViewModel.kt
+++ app/src/main/java/com/example/tobusytodie/viewModel/RegisterViewModel.kt
@@ -1,5 +1,53 @@
 package com.example.tobusytodie.viewModel
 
-class RegisterViewModel {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+
+class RegisterViewModel : ViewModel() {
+    //publisher para el loader
+
+    private val _loaderState = MutableLiveData<Boolean>()
+    val loaderState: LiveData<Boolean>
+        get() = _loaderState
+
+    //publisher para el registro
+    private val _validRegister = MutableLiveData<Boolean>()
+    val validRegister: LiveData<Boolean>
+        get() = _validRegister //obtiene boolean de register
+
+    private val firebase = FirebaseAuth.getInstance() //instancia
+
+    //funcion para registrar
+    fun requestSignUp(email: String, password: String) {
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            _loaderState.value = true //muestra loader por que comienza corrutina
+
+            viewModelScope.launch { //inicia corrutina
+                val result = firebase.createUserWithEmailAndPassword(email, password).await()
+                _loaderState.value = false //cuando termina corrutina oculta loader
+                //mensajes para verificar estado de registro
+                result.user?.let {
+                    delay(5000)
+                    Log.i("Firebase", "Se creó al usuario con éxito.")
+                    _validRegister.value = true
+                } ?: run {
+                    delay(5000)
+                    Log.e("Firebase", "Ocurrio un problema al crear al usuario.")
+
+                }
+            }
+        } else {
+            _validRegister.value = false
+        }
+    }
+
+
 
 }
