75295dff6a12ed823127208a9299bbbfc06f4855
Tony0619-29 <antoniomed@comunidad.unam.mx>
2025-06-10 14:26:33 -0600

    Implementacion de CRUD

---
+++ app/src/main/java/com/example/tobusytodie/model/Tarea.kt
@@ -0,0 +1,10 @@
+package com.example.tobusytodie.model
+
+import java.util.Date
+
+data class Tarea (
+    val id: String = "",
+    val name: String = "",
+    val description: String = "",
+    val date: Date = Date()
+)
--- app/src/main/java/com/example/tobusytodie/network/TareaRepository.kt
+++ app/src/main/java/com/example/tobusytodie/network/TareaRepository.kt
@@ -1,5 +1,51 @@
 package com.example.tobusytodie.network
 
+import com.example.tobusytodie.core.ResultWrapper
+import com.example.tobusytodie.core.safeCall
+import com.example.tobusytodie.model.Tarea
+import com.google.firebase.firestore.FirebaseFirestore
+import jakarta.inject.Inject
+import kotlinx.coroutines.tasks.await
 
-class TareaRepository {
+
+class TareaRepository @Inject constructor(
+
+    private val firestore: FirebaseFirestore
+) {
+    private val tareasCollection = firestore.collection("Tareas")
+
+    suspend fun createTarea(tarea: Tarea): ResultWrapper<Void> = safeCall {
+        tareasCollection.document(tarea.id).set(tarea).await()
+    }
+
+    suspend fun getTareas(id: String? = null): ResultWrapper<Any> = safeCall {
+        if (id != null) {
+            val doc = tareasCollection.document(id).get().await()
+            doc.toObject(Tarea::class.java) ?: throw Exception("Tarea no encontrada")
+        } else {
+            val snapshot = tareasCollection.get().await()
+            snapshot.toObjects(Tarea::class.java)
+        }
+    }
+
+    /*
+            * suspend fun getTareaById(id: String): ResultWrapper<Tarea> = safeCall {
+            val doc = tareasCollection.document(id).get().await()
+            doc.toObject(Tarea::class.java) ?: throw Exception("Tarea no encontrada")
+        }
+
+        suspend fun getAllTareas(): ResultWrapper<List<Tarea>> = safeCall {
+            val snapshot = tareasCollection.get().await()
+            snapshot.toObjects(Tarea::class.java)
+        }
+        */
+
+
+    suspend fun updateTarea(tarea: Tarea): ResultWrapper<Void> = safeCall {
+        tareasCollection.document(tarea.id).set(tarea).await()
+    }
+    suspend fun deleteTarea(id: String): ResultWrapper<Void> = safeCall {
+        tareasCollection.document(id).delete().await()
+    }
+
 }
